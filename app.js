32909dea08aadfd779f44d521a478a52b3cb2582
Bradley Donn Kukuk <brad@bdksoftware.com>
2022-06-20 12:56:48 -0700

    Final Changes

--- app.js
+++ app.js
@@ -4,12 +4,11 @@ function init() {
 
     // Add event Listener for CLick
     btn.addEventListener('click', () => {
-        let city = document.getElementById("input1").value;
-        let state = document.getElementById("input2").value;
-        let zip = document.getElementById("input3").value;
+        
+        let zip = document.getElementById('input1').value;
 
         // Checks if Location value is false/null/blank
-        if(!city || !zip || !state)
+        if(!zip)
         {
             alert("All Fields Are Required");
             return;
@@ -18,40 +17,13 @@ function init() {
         // Checks if btn is value, if so it will save the city and state values
             // and route the user to the weather.html page that displays the results
         if(btn) {
-            
-            localStorage.setItem('city', city);
-            localStorage.setItem('zip', zip);
-            localStorage.setItem('state', state);
+            sessionStorage.setItem('zip', zip);
             window.document.location = './weather.html';
             return;
         }
     })
 }
 
-// Fetch Request to API for Current Weather Information
-
-// RETURN VALUE: JSON data
-function getWeather() {
-
-};
-
-
-// Gets Lat and Lon values and returns them, if their is no return the user is prompted to
-    // re-input values that will work with OpenWeatherAPI
-
-// RETURN VALUE: Lat and Long
-function checkLocationValues() {
-
-};
-
-
-// Gets the time of where the user is located
-
-// RETURN VALUE: Time Stamp for Users Locations
-// This will be used to change the background image/color depending on the time
-function getTime() {
-
-}
 
 document.addEventListener("DOMContentLoaded", function() {
     init();
--- weather.js
+++ weather.js
@@ -1,16 +1,85 @@
 function init() {
-    let spn = document.querySelector("#userLocation");
-    let city = localStorage.getItem('city');
-    let state = localStorage.getItem('state');
+    // Get values from Local Storage
+    
+        let zipCode = sessionStorage.getItem('zip');
+        fetch('https://api.openweathermap.org/data/2.5/weather?zip='+
+        zipCode+',us&appid=f63d273f25119d02e39ddf003ee6e1fa&units=imperial').then(response => response.json()).then(
+            data => weatherData = data
+        ).then(() => {
+            setLocation(weatherData.name);
+            setIcon(weatherData.weather[0].icon);
+            setDescription(weatherData.weather[0].description);
+            setTemps(weatherData.main.temp_min, weatherData.main.temp, weatherData.main.temp_max)
+            setWind(weatherData.wind.speed);
+        });
+    };
 
+    // Inject Values into HTML
+// Sets Icon from sessionStorage Value
+function setIcon(icon) {
+    let img = document.querySelector('#icon');
 
-    if(spn)
-    {
-       
-        spn.innerHTML = city + ", " + state;
-    }
+    // Guard Clause
+    if(!img) return;
+
+    img.setAttribute("src", `http://openweathermap.org/img/wn/${icon}.png`);
+    img.setAttribute("alt", "Icon for Weather Data");
 }
 
+// Sets Location Values from sessionStorage Value
+function setLocation(location)
+{
+    let title = document.querySelector('#container-header');
+
+    // Guard Clause
+    if(!title) return;
+
+    title.innerHTML = location;
+    
+    
+}
+
+// Sets Description From sessionStorage Value
+function setDescription(description)
+{
+    let text = document.querySelector("#description");
+
+    // Guard Clause
+    if(!text) return;
+
+    text.innerHTML = description.toUpperCase();
+    
+}
+
+
+// Sets Temp values from sessionStorage
+function setTemps(temp_min, temp, temp_max)
+{
+    let low = document.querySelector('.low');
+    let current = document.querySelector('.current');
+    let high = document.querySelector('.high');
+
+    // Guard Clause
+    if(!low && !current && !high) return;
+   
+
+    low.innerHTML = temp_min + ' \u00B0';
+    current.innerHTML = temp + ' \u00B0';
+    high.innerHTML = temp_max + ' \u00B0';
+}
+
+// Sets Wind Value from LocalStoreage
+function setWind(wind)
+{
+    let conditions = document.querySelector('#wind');
+
+    // Guard Clause
+    if(!conditions) return;
+
+    conditions.innerHTML = wind + ' MPH';
+}
+
+// Calls Init when the DOM content is loaded on screen
 document.addEventListener("DOMContentLoaded", function() {
     init();
 })
